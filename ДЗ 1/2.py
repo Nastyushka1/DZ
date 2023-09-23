0d5dd6cb4b8643594f55f126303d5578025cdd00
Nastyushka1 <108815597+Nastyushka1@users.noreply.github.com>
2023-09-23 10:52:25 +0300

    дз 1

---
+++ ДЗ 1/2.py
@@ -0,0 +1,10 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+f = float(input())
+if f - d == 0:
+    print('Делить на ноль нельзя!')
+else:
+    v = (a * b - c) / (f - d)
+    print(v)
