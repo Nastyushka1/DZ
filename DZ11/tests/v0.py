a4bc5fa71df60a029e9601c69bf8ec2ed631de8f
Nastyushka1 <108815597+Nastyushka1@users.noreply.github.com>
2024-02-24 16:44:17 +0300

    v0.py

--- DZ11/tests/v0.py
+++ DZ11/tests/v0.py
@@ -1,10 +1,23 @@
 class Fraction:
-    def __init__(self, num: float = 1, den: float = 1):
-        self.numerator = num
-        self.denominator = den
+    def __init__(self, numerator=0, denominator=1):
+        self.numerator, self.denominator = self.validate(numerator, denominator)
 
-    def input_digits(self):
-        self.numerator, self.denominator = map(float, input("Введите числитель и знаменатель через пробел: ").split(' '))
+    def input_fraction(self):
+        self.numerator, self.denominator = self.validate(*input("Введите числитель и знаменатель через пробел: ").split(' '))
 
     def __str__(self):
-        return f'{self.numerator}/{self.denominator}'
+        return f"{self.numerator}/{self.denominator}"
+
+    @staticmethod
+    def validate(num, den):
+        if den == 0:
+            raise ZeroDivisionError("Знаменатель не может быть равен нулю")
+        if str(num).isdigit() and str(den).isdigit():
+            return int(num), int(den)
+        raise ValueError('Неверный формат данных')
+
+
+f = Fraction(12, 13)
+f.validate(12, 1)
+f.input_fraction()
+print(type(f.numerator))
